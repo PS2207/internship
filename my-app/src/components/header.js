fd0917bfc1462216244bf10710ea70174fb3a106
PS2207 <pragyasinha2207@gmail.com>
2021-12-12 17:08:39 +0530

    header done

---
+++ my-app/src/components/header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const Header=()=>{
+    return(
+        <div className="headerStyle">
+           <h1>Query Appliaction</h1>
+        </div>
+    )
+
+}
+export default Header;
