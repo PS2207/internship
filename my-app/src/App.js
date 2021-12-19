ee9119ee962c10d6ccbcdd2462ceb7816cc5927c
PS2207 <pragyasinha2207@gmail.com>
2021-12-19 12:00:13 +0530

    header,nav,home,student,footer done

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,17 @@
 // import logo from './logo.svg';
 import './App.css';
 import Header from './components/header';
+import Footer from './components/footer';
+import Home from './components/home';
+import Navbar from './components/navbar';
 function App() {
   return (
-  
     <div className="App">
-      <Header/>
+     <Header/>
+     <Navbar/>
+     <Home/>
+     <Footer/>
     </div>
-  
   );
 }
 
---
+++ my-app/src/components/footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function footer() {
+    return (
+        <div className="footerStyle">
+           <h1>copyRight@ yuktisoftware</h1> 
+        </div>
+    )
+}
+
+export default footer
