37009bd24b201b8c70ec1b551fd65ee04777824e
PS2207 <pragyasinha2207@gmail.com>
2021-12-19 12:35:33 +0530

    header,nav,home,footer done

--- my-app/src/components/home.js
+++ my-app/src/components/home.js
@@ -6,13 +6,13 @@ function Home() {
     <div className="homeStyle">
       <div className="homeTableStyle">
         <Student name="Pragya" marks="91" mobile="+91 123452340" />
-        <Student name="Shivji" marks="92" mobile="+91 1234567890" />
-        <Student name="Radhe" marks="92" mobile="+91 654789123" />
+        <Student name="Komal" marks="92" mobile="+91 1234567890" />
+        <Student name="Sandhya" marks="92" mobile="+91 654789123" />
       </div>
       <div className="homeFormStyle">
         <Student name="Pragya" marks="91" mobile="+91 123452340" />
-        <Student name="Shivji" marks="92" mobile="+91 1234567890" />
-        <Student name="Radhe" marks="92" mobile="+91 654789123" />
+        <Student name="Komal" marks="92" mobile="+91 1234567890" />
+        <Student name="Sandhya" marks="92" mobile="+91 654789123" />
       </div>
     </div>
   );
